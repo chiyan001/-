0acf70ca93a534cdf207705628b4148c24f42816
神丶渐入嘉静 <qianfeng@Godlike.local>
2015-07-23 10:56:55 +0800

    Initial Commit

---
+++ 字符串的常用方法/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  字符串的常用方法
+//
+//  Created by 神丶渐入嘉静 on 15-7-23.
+//  Copyright (c) 2015年 LPC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
